4955414ccf9670d31a4b5023acd39cd4794f458a
Dragos Bogdan <dragos.bogdan@analog.com>
2021-11-17 09:35:01 +0200

    Add support for ADALM-PLUTO-NG
    The ADALM-PLUTO-NG Active Learning Module is an easy to use tool
    available from Analog Devices, Inc. (ADI) that can be used to
    introduce fundamentals of Software Defined Radio (SDR) or Radio
    Frequency (RF) or Communications as advanced topics in electrical
    engineering in a self or instructor lead setting.
    
    Signed-off-by: Dragos Bogdan <dragos.bogdan@analog.com>

---
+++ include/configs/zynqmp_pluto_ng.h
@@ -0,0 +1,18 @@
+/*
+ * Configuration for Analog Devices, Inc. Pluto NG
+ *
+ * (C) Copyright 2021 Analog Devices, Inc.
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+#ifndef __CONFIG_ZYNQMP_PLUTO_NG_H
+#define __CONFIG_ZYNQMP_PLUTO_NG_H
+
+#define CONFIG_ZYNQ_SDHCI1
+
+#define CONFIG_SYS_I2C_ZYNQ
+
+#include <configs/xilinx_zynqmp.h>
+
+#endif /* __CONFIG_ZYNQMP_PLUTO_NG_H */
